58d5d2f82a8b9c8203e4ef9ec31723d6550d5dc2
Muhammad Bintang Nugraha <djakarta1973@gmail.com>
2026-02-20 14:20:38 +0700

    feat: add devtools_options.yaml for DevTools settings and enhance main.dart with improved app structure and routing

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,9 +1,20 @@
+import 'package:d_session/d_session.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:frontend/common/app_color.dart';
+import 'package:frontend/common/app_route.dart';
+import 'package:frontend/data/models/user.dart';
 import 'package:frontend/presentation/bloc/user/user_cubit.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
-  runApp(const MainApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]).then((
+    value,
+  ) {
+    runApp(const MainApp());
+  });
 }
 
 class MainApp extends StatelessWidget {
@@ -13,12 +24,56 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: [BlocProvider(create: (context) => UserCubit())],
-      child: const MaterialApp(
-        home: Scaffold(
-          body: Center(
-            child: Text('Hello World!'),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.light(useMaterial3: true).copyWith(
+          primaryColor: AppColor.primary,
+          colorScheme: ColorScheme.light(
+            primary: AppColor.primary,
+            secondary: AppColor.secondary,
+          ),
+          scaffoldBackgroundColor: AppColor.scaffold,
+          textTheme: GoogleFonts.poppinsTextTheme(),
+          appBarTheme: AppBarTheme(
+            surfaceTintColor: AppColor.primary,
+            backgroundColor: AppColor.primary,
+            foregroundColor: Colors.white,
+            titleTextStyle: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 18,
+            ),
+          ),
+          popupMenuTheme: const PopupMenuThemeData(
+            color: Colors.white,
+            surfaceTintColor: Colors.white,
+          ),
+          dialogTheme: const DialogThemeData(
+            surfaceTintColor: Colors.white,
+            backgroundColor: Colors.white,
           ),
         ),
+        initialRoute: AppRoute.home,
+        routes: {
+          AppRoute.home: (context) {
+            return FutureBuilder(
+              future: DSession.getUser(),
+              builder: (context, snapshot) {
+                if (snapshot.data == null) return const Scaffold();
+                User user = User.fromJson(Map.from(snapshot.data!));
+                context.read<UserCubit>().update(user);
+                if (user.role == 'Admin') return const Scaffold();
+                return const Scaffold();
+              },
+            );
+          },
+          AppRoute.addEmployee: (context) => const Scaffold(),
+          AppRoute.addTask: (context) => const Scaffold(),
+          AppRoute.detailTask: (context) => const Scaffold(),
+          AppRoute.listTask: (context) => const Scaffold(),
+          AppRoute.login: (context) => const Scaffold(),
+          AppRoute.monitorEmployee: (context) => const Scaffold(),
+          AppRoute.profile: (context) => const Scaffold(),
+        },
       ),
     );
   }
