2f5016401e703852b5a15d015dbb3fe6096ac45c
Muhammad Bintang Nugraha <djakarta1973@gmail.com>
2026-02-20 13:45:01 +0700

    feat: integrate UserCubit with MultiBlocProvider and update API host URL

--- frontend/lib/common/urls.dart
+++ frontend/lib/common/urls.dart
@@ -1,4 +1,4 @@
 class URLs {
-  static const host = 'http://192.168.43.221:8080';
+  static const host = 'http://localhost:8080';
   static String image(String fileName) => '$host/attachments/$fileName';
 }
--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:frontend/presentation/bloc/user/user_cubit.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,10 +11,13 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MultiBlocProvider(
+      providers: [BlocProvider(create: (context) => UserCubit())],
+      child: const MaterialApp(
+        home: Scaffold(
+          body: Center(
+            child: Text('Hello World!'),
+          ),
         ),
       ),
     );
