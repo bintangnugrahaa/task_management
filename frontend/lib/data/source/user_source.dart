f7ac2118fc8731d02bc00415d3dce4a9e24f33e8
Muhammad Bintang Nugraha <djakarta1973@gmail.com>
2026-02-20 14:25:13 +0700

    feat: implement user login functionality in UserSource class with API integration

---
+++ frontend/lib/data/source/user_source.dart
@@ -0,0 +1,31 @@
+import 'dart:convert';
+
+import 'package:d_method/d_method.dart';
+import 'package:frontend/common/urls.dart';
+import 'package:frontend/data/models/user.dart';
+import 'package:http/http.dart' as http;
+
+class UserSource {
+  static const _baseURL = '${URLs.host}/users';
+
+  static Future<User?> login(String email, String password) async {
+    try {
+      final response = await http.post(
+        Uri.parse('$_baseURL/login'),
+        body: jsonEncode({"email": email, "password": password}),
+      );
+
+      DMethod.logResponse(response);
+
+      if (response.statusCode == 200) {
+        Map resBody = jsonDecode(response.body);
+        return User.fromJson(Map.from(resBody));
+      }
+
+      return null;
+    } catch (e) {
+      DMethod.log(e.toString(), colorCode: 1);
+      return null;
+    }
+  }
+}
